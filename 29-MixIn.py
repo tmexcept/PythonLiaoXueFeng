3c12846fd581274ad9a0220153867f0a867be645
tmexcept <200809201230_2008@163.com>
2018-08-07 11:07:21 +0800

    .idea文件添加到ignore

--- 29-MixIn.py
+++ 29-MixIn.py
@@ -1,33 +1,54 @@
+# Python允许使用多重继承，因此，MixIn就是一种常见的设计
+
 class Runnable(object):
-	def run(self):
-		print('Running...')
+    def run(self):
+        print('Running...')
+
 
 class Flyable(object):
-	def fly(self):
-		print('Flying...')
+    def fly(self):
+        print('Flying...')
 
 class Animal(object):
     pass
+
 
 # 大类:
 class Mammal(Animal):
     pass
 
+
 class Bird(Animal):
     pass
 
+
 # 各种动物:
-class Dog(Mammal,Runnable):
+# 多重继承，一个子类就可以同时获得多个父类的所有功能
+class Dog(Mammal, Runnable):
     pass
+
 
 class Bat(Mammal):
     pass
 
+
 class Parrot(Bird):
     pass
+
 
 class Ostrich(Bird):
     pass
 
+
 d = Dog()
 d.run()
+
+# 在设计类的继承关系时，通常，主线都是单一继承下来的，例如，Ostrich继承自Bird。
+# 但是，如果需要“混入”额外的功能，通过多重继承就可以实现，比如，让Ostrich除了继承自Bird外，再同时继承Runnable。这种设计通常称之为MixIn。
+# 为了更好地看出继承关系，我们把Runnable和Flyable改为RunnableMixIn和FlyableMixIn。
+# 类似的，你还可以定义出肉食动物CarnivorousMixIn和植食动物HerbivoresMixIn，让某个动物同时拥有好几个MixIn：
+
+# 比如，编写一个多进程模式的TCP服务，定义如下：
+class MyTCPServer(TCPServer, ForkingMixIn):
+    pass
+
